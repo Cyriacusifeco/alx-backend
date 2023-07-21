d62954d28ca52fed9ac497767abc5b390c4cf4fc
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-21 14:23:51 +0000

    implement hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -65,3 +65,29 @@ class Server:
             return []
 
         return dataset[start_index:end_index]
+
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        Return a dictionary with hypermedia pagination information.
+
+        Args:
+            page (int): The 1-indexed page number. Default is 1.
+            page_size (int): The number of items per page. Default is 10.
+
+        Returns:
+            dict: A dictionary containing hypermedia pagination information.
+        """
+        page_data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(page_data),
+            "page": page,
+            "data": page_data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+        }
