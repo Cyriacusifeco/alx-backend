e9f27c1e349a39c1e4f05526d2f69a122148b826
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-08-01 13:28:28 +0000

    basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Flask App - Basic
+
+This module sets up a basic Flask app with a single route.
+The route renders an index.html template with the title and header.
+
+Usage:
+    Run this script to start the Flask app.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() -> str:
+    """
+    Index route handler.
+
+    Returns:
+        str: The HTML content to display on the page.
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(port=3000)
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,1 @@
+BACKEND: i18n
